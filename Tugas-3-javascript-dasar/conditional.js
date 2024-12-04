e91d43bf3391282febd9ee063ab07ae2bdb40fca
Musyafa095 <musyafa095@gamil.com>
2024-12-04 13:16:28 +0700

    Tugas-3-javascript-dasar

---
+++ Tugas-3-javascript-dasar/conditional.js
@@ -0,0 +1,14 @@
+var nama = "juned";
+var peran = "werewolf";
+
+if(nama == "" && peran == ""){
+  console.loh("nama harus diisi!")
+} else if (nama == "john" && peran == ""){
+  console.log(`Halo ${nama}  Pilih peranmu untuk memulai game!`)
+} else if (nama == "jane" && peran == "penyihir"){
+  console.log(`Selamat datang di Dunia Werewolf, ${nama} \n Hallo penyihir ${nama}, kamu dapat melihat siapa siapa yang menjadi werewolf!`)
+} else if (nama == "jenita" && peran == guard ){
+  console.log(`Selamat datang di Dunia Werewolf, ${nama} \n Hallo guard ${nama}, kamu akan membantu melindungi temanmu dari serangan werewolf!`)
+} else if (nama == "juned" && peran == "werewolf"){
+  console.log(`Selamat datang di Dunia Werewolf, ${nama} \n Hallo werewolf ${nama}, Kamu akan memakan mangsa setiap malam!"`)
+}
