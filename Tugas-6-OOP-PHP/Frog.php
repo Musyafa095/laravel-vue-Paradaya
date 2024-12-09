f1d26d32c4c2dd75bf18af8721666f9ebff3a390
Musyafa095 <musyafa095@gamil.com>
2024-12-09 12:15:39 +0700

    Done task OOP PHP

---
+++ Tugas-6-OOP-PHP/Frog.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+require_once 'Animal.php';
+
+class Frog extends Animal {
+    public function jump() {
+        echo "Jump: Hop-Hop;)<br>";
+    }
+    public function display() {
+        parent::display();
+        $this->jump();
+    }
+}
+  ?>
+  
+</body>
+</html>
