6b2ea1b0a7c15fd58dcb5481a98ca3667c4dfec9
Musyafa095 <musyafa095@gamil.com>
2024-12-12 23:51:41 +0700

    Done task Laravel-templating

---
+++ Tugas-9-Laravel-Templating/example-app/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\AuthController;
+
+Route ::get('/', [IndexController::class, 'home']);
+Route ::get('/register', [AuthController::class, 'register']);
+Route ::post('/welcome', [AuthController::class, 'welcome']);
+
+Route ::get('/master', function(){
+  return view('layouts.master');
+});
+
+Route ::get('/data-table', [IndexController::class, 'table']);
