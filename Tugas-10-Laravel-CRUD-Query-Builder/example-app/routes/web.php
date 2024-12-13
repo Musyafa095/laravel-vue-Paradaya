0acf591f6b7d4c2f97771f4060588b8d1fe8467f
Musyafa095 <musyafa095@gamil.com>
2024-12-14 01:03:16 +0700

    tugas crud & query

--- Tugas-10-Laravel-CRUD-Query-Builder/example-app/routes/web.php
+++ Tugas-10-Laravel-CRUD-Query-Builder/example-app/routes/web.php
@@ -14,22 +14,3 @@ Route ::get('/master', function(){
 });
 
 Route ::get('/data-table', [IndexController::class, 'table']);
-
-
-//C Create data
-Route::get('/book/create', [BookController::class, 'create']);
-//Route simpan data
-Route::post('/book', [BookController::class, 'store']);
-
-//R Read data
-Route::get('/book', [BookController::class, 'index']);
-//R detail id
-Route::get('/book/{book_id}', [BookController::class, 'show']);
-//Update 
-//Route=> form edit
-Route::get('/book/{book_id}/edit', [BookController::class, 'edit']);
-//update data id
-Route::put('/book/{book_id}', [BookController::class, 'update' ]);
-//Delete
-Route::delete('/book/{book_id}', [BookController::class, 'destroy']);
-
