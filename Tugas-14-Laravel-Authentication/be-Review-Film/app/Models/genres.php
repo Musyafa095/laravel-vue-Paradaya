196fe3eeb69d20d022f5879368f136e5ce33f336
Musyafa095 <musyafa095@gamil.com>
2024-12-19 10:45:32 +0700

    tugas jwt

---
+++ Tugas-14-Laravel-Authentication/be-Review-Film/app/Models/genres.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Support\Str;
+class genres extends Model
+{
+    use HasFactory, HasUuids;
+    protected $table =  'genres';
+    protected $fillable =  ['name'];
+    protected $keyType =  'string';
+    public $incrementing =  false;
+  
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+}
